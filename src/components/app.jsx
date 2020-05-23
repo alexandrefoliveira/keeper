64a48c31a9807c310bdb347d4307a10960a2f830
Alexandre Oliveira <alexandreoliveira@Alexandres-MacBook-Pro.local>
2020-05-23 16:26:50 -0400

    Second commit using map, and anonymous function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import notes from "./notes";
+
 
 function App(){
     return <div>
         <Header />
+        {notes.map((noteItem) => <Note
+        key = {noteItem.key} 
+        title = {noteItem.title}
+        content = {noteItem.content}
+    />
+)}  
         <Footer />
-        <Note />        
+                 
     </div>
     
-
 }
 
 export default App;
