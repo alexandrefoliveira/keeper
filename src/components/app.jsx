0f8b88d8ff575b5ca1dd7a024289c831cdbd83f4
Alexandre Oliveira <alexandreoliveira@Alexandres-MacBook-Pro.local>
2020-06-02 19:37:57 -0400

    It is implemented to Add title and content, and now it is possible to delete title, and content

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
-import notes from "./notes";
 
 
-function App(){
-    return <div>
-        <Header />
-        {notes.map((noteItem) => <Note
-        key = {noteItem.key} 
-        title = {noteItem.title}
-        content = {noteItem.content}
-    />
-)}  
-        <Footer />
-                 
+import CreateArea from "./CreateArea";
+
+function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        })
+    }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote}/>
+       { notes.map((noteItem, index) => {
+            return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
+        })}
+      <Footer />
     </div>
-    
+  );
 }
 
 export default App;
